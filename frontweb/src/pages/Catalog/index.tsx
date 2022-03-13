22ec4a485b12e5ae9f6c2a4e9f7b216e3ad48f79
Deividwb <bernardinodeivid@gmail.com>
2022-03-13 12:50:46 -0300

    Back end

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -19,6 +19,9 @@ const Catalog = () => {
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <ProductCard />
         </div>
+        <div className="col-sm-6 col-lg-4 col-xl-3">
+          <ProductCard />
+        </div>
       </div>
     </div>
   );
